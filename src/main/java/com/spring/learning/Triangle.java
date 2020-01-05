b41cdfcf89d6868e0f8ddefbd853379c16999d44
Tarun Chopra <tchopra@catalystone.com>
2020-01-05 13:01:55 +0530

    Primitive property initialization

--- src/main/java/com/spring/learning/Triangle.java
+++ src/main/java/com/spring/learning/Triangle.java
@@ -2,7 +2,17 @@ package com.spring.learning;
 
 public class Triangle {
 
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public void draw() {
-        System.out.println("Triangle drawn");
+        System.out.println("Triangle drawn with type '" + this.getType() + "'");
     }
 }
