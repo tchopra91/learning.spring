70124bb75b77350e3d10e55a7c8e834ef7064ca3
Tarun Chopra <tchopra@catalystone.com>
2020-01-05 13:06:44 +0530

    Primitive property initialization using constructor injection

--- src/main/java/com/spring/learning/Triangle.java
+++ src/main/java/com/spring/learning/Triangle.java
@@ -4,13 +4,17 @@ public class Triangle {
 
     private String type;
 
+    public Triangle(String type) {
+        this.type = type;
+    }
+
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+    // public void setType(String type) {
+    // this.type = type;
+    // }
 
     public void draw() {
         System.out.println("Triangle drawn with type '" + this.getType() + "'");
