25053135020bd22249e7b818787ff61455d85d64
Tarun Chopra <tchopra@catalystone.com>
2020-01-05 12:52:10 +0530

    Setup and fetch bean instance using application context

--- src/main/java/com/spring/learning/DemoApplication.java
+++ src/main/java/com/spring/learning/DemoApplication.java
@@ -1,23 +1,18 @@
 package com.spring.learning;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("Running main method of DemoApplication");
 
-        // Preparing resource to read spring.xml file.
-        Resource resource = new ClassPathResource("spring.xml");
-
         // Preparing core context using bean factory to initialize spring.
-        BeanFactory factory = new XmlBeanFactory(resource);
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
         // Getting bean instance from factory.
-        Triangle triangle = (Triangle) factory.getBean("triangle");
+        Triangle triangle = (Triangle) context.getBean("triangle");
         triangle.draw();
     }
 
