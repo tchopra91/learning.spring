83e324b6c3b5a4e2e398f7ac956738761dd89df1
Tarun Chopra <tchopra@catalystone.com>
2020-01-05 12:48:36 +0530

    Setup and fetch bean instance using bean factory

--- src/main/java/com/spring/learning/DemoApplication.java
+++ src/main/java/com/spring/learning/DemoApplication.java
@@ -1,9 +1,24 @@
 package com.spring.learning;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("Running main method of DemoApplication");
+
+        // Preparing resource to read spring.xml file.
+        Resource resource = new ClassPathResource("spring.xml");
+
+        // Preparing core context using bean factory to initialize spring.
+        BeanFactory factory = new XmlBeanFactory(resource);
+
+        // Getting bean instance from factory.
+        Triangle triangle = (Triangle) factory.getBean("triangle");
+        triangle.draw();
     }
 
 }
