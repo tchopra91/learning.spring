33449b6263a4a82d3d21b41a308643a9645940cc
Tarun Chopra <tchopra@catalystone.com>
2020-01-05 15:37:31 +0530

    Collection property initialization using constructor injection

--- src/main/java/com/spring/learning/Triangle.java
+++ src/main/java/com/spring/learning/Triangle.java
@@ -2,38 +2,17 @@ package com.spring.learning;
 
 public class Triangle {
 
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
+    private Point[] points;
 
-    public Point getPointA() {
-        return pointA;
-    }
-
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
+    public Triangle(Point[] points) {
+        this.points = points;
     }
 
     public void draw() {
         System.out.println("Triangle points::");
-        System.out.println("Point A (" + this.getPointA().getX() + ", " + this.getPointA().getY() + ")");
-        System.out.println("Point B (" + this.getPointB().getX() + ", " + this.getPointB().getY() + ")");
-        System.out.println("Point C (" + this.getPointC().getX() + ", " + this.getPointC().getY() + ")");
+
+        for (Point point : points) {
+            System.out.println("Point (" + point.getX() + ", " + point.getY() + ")");
+        }
     }
 }
