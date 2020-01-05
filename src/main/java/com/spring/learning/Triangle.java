cb5958d791d50fc14148f745bb194ccd54d3d64d
Tarun Chopra <tchopra@catalystone.com>
2020-01-05 13:27:42 +0530

    Inner bean for property initialization using property injection

--- src/main/java/com/spring/learning/Triangle.java
+++ src/main/java/com/spring/learning/Triangle.java
@@ -6,35 +6,29 @@ public class Triangle {
     private Point pointB;
     private Point pointC;
 
-    public Triangle(Point pointA, Point pointB, Point pointC) {
-        this.pointA = pointA;
-        this.pointB = pointB;
-        this.pointC = pointC;
-    }
-
     public Point getPointA() {
         return pointA;
     }
 
-    // public void setPointA(Point pointA) {
-    // this.pointA = pointA;
-    // }
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
 
     public Point getPointB() {
         return pointB;
     }
 
-    // public void setPointB(Point pointB) {
-    // this.pointB = pointB;
-    // }
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
 
     public Point getPointC() {
         return pointC;
     }
 
-    // public void setPointC(Point pointC) {
-    // this.pointC = pointC;
-    // }
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
 
     public void draw() {
         System.out.println("Triangle points::");
