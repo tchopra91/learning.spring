a71ddd875a6398524b78e2ca1c5f8ea8f463e2e6
Tarun Chopra <tchopra@catalystone.com>
2020-01-05 12:43:35 +0530

    Resetting application code to start from scratch

--- src/main/java/com/spring/learning/DemoApplication.java
+++ src/main/java/com/spring/learning/DemoApplication.java
@@ -1,18 +1,9 @@
 package com.spring.learning;
 
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class DemoApplication {
 
     public static void main(String[] args) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        // context.registerShutdownHook();
-
-        Triangle triangle = (Triangle) context.getBean("triangle");
-        triangle.draw();
-
-        context.close();
+        System.out.println("Running main method of DemoApplication");
     }
 
 }
