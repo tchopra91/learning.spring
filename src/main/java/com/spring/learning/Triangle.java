1e8870bd7eb25106243ccbf5d9497f4fb6fb1a83
Tarun Chopra <tchopra@catalystone.com>
2020-01-07 10:16:13 +0530

    Implemented @Component annotation

--- src/main/java/com/spring/learning/Triangle.java
+++ src/main/java/com/spring/learning/Triangle.java
@@ -5,7 +5,9 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Triangle {
 
     private Point pointA;
